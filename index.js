9bd3859869ca3b78ea9dc09b2aefdb360f6eb9c3
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-04-26 22:25:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+//source: https://attacomsian.com/blog/javascript-array-lowercase-uppercase
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching (drivers, string){
+    const lowercased = drivers.filter(drivers => drivers.toLowerCase() === string.toLowerCase());
+    return lowercased;
+}
+//    fuzzyMatch()
+function fuzzyMatch(drivers, string) {
+    const letermatch = drivers.filter(drivers => drivers[0] === string[0]);
+    return letermatch;
+}
+
+//       matchName()
+
+function matchName (people, string) {
+    const sameNameFc = people.filter(driver => driver.name === string);
+    
+//    const objectNameFunction = array.filter(driver => driver.name === string);
+
+    return sameNameFc;
+}
